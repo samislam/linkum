bb2f4aefeb5432086e18636db0e19b4eb207bdef
Islam Yamor <islamyamor@gmail.com>
2024-10-20 22:42:47 +0300

    [web] created the sign up `/auth/sign-up` page

---
+++ apps/web/src/components/composables/sign-up-form.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React from 'react'
+import { ArrowRight } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+
+const SignUpForm = () => {
+  const { register, handleSubmit } = useForm()
+  return (
+    <form className="space-y-4" onSubmit={handleSubmit((data: any) => console.log(data))}>
+      <div className="space-y-2">
+        <Label htmlFor="name" className="text-white">
+          Name
+        </Label>
+        <Input
+          id="name"
+          placeholder="John Doe"
+          {...register('name')}
+          className="border-white/30 bg-white/20 text-white placeholder-white/50"
+        />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="email" className="text-white">
+          Email
+        </Label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="you@example.com"
+          {...register('email')}
+          className="border-white/30 bg-white/20 text-white placeholder-white/50"
+        />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="password" className="text-white">
+          Password
+        </Label>
+        <Input
+          id="password"
+          type="password"
+          {...register('password')}
+          className="border-white/30 bg-white/20 text-white placeholder-white/50"
+        />
+      </div>
+      <Button className="w-full bg-white text-teal-600 hover:bg-white/90">
+        Sign Up
+        <ArrowRight className="ml-2 h-4 w-4" />
+      </Button>
+    </form>
+  )
+}
+
+export default SignUpForm
