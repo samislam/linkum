737e076391626fb60f3d66f00d1940056f332c82
Islam Yamor <islamyamor@gmail.com>
2024-10-22 23:34:39 +0300

    [api] created additional utilities

--- apps/api/src/utils/misc-utils.ts
+++ apps/api/src/utils/misc-utils.ts
@@ -1,4 +1,6 @@
-import { ValueTransformer } from 'typeorm'
+import { ExtractEntity } from '@/types/misc-types'
+import { Repository, ValueTransformer } from 'typeorm'
+import { ColumnMetadata } from 'typeorm/metadata/ColumnMetadata'
 
 /**
  * Since TypeORM requires the value transformer to include both the `to` & the `from` callbacks,
@@ -8,3 +10,22 @@ export const from = <R = any, V = any>(fromFn: (val: V) => R): ValueTransformer 
   to: (val: any) => val,
   from: fromFn,
 })
+
+/**
+ * This function is used to get the columns metadata and column names from a typeorm repository,
+ * it's useful when you want for example to query with the find method when using the `select` flag
+ */
+export const getColumns = <E extends object, T extends Repository<E> = Repository<E>>(
+  repo: T,
+  excludeNotSelected = false
+) => {
+  let columns: ColumnMetadata[] = repo.metadata.columns
+  if (excludeNotSelected) columns = columns.filter((col) => col.isSelect)
+  return {
+    columns,
+    get names() {
+      type InferredE = ExtractEntity<T>
+      return columns.map((col) => col.propertyName as keyof InferredE)
+    },
+  }
+}
