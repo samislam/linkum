8adf58af533db7ecf1c112682690beab631d1cf4
Islam Yamor <islamyamor@gmail.com>
2024-10-22 14:23:46 +0300

    [api ]setup validation pipe and displayed general message upon service start

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import * as chalk from 'chalk'
+import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core'
+import { formatUrl } from './utils/formatUrl'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
+import { EnvironmentVars } from './types/environment-vars'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  const configService = app.get(ConfigService<EnvironmentVars, true>)
+  const HOST = configService.get('HOST', { infer: true })
+  const PORT = configService.get('PORT', { infer: true })
+  await app.listen(PORT, HOST, () => {
+    const url = formatUrl(HOST, PORT)
+    console.log(`\nService listening on ${chalk.bold.underline(url)}\n`)
+  })
 }
-bootstrap();
+bootstrap()
