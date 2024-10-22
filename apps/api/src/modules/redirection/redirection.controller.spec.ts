836e1f4b8ee5981fe11df29afed26239d597e944
Islam Yamor <islamyamor@gmail.com>
2024-10-22 17:53:59 +0300

    [api] formatted the code

--- apps/api/src/modules/redirection/redirection.controller.spec.ts
+++ apps/api/src/modules/redirection/redirection.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { RedirectionController } from './redirection.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+import { RedirectionController } from './redirection.controller'
 
 describe('RedirectionController', () => {
-  let controller: RedirectionController;
+  let controller: RedirectionController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RedirectionController],
-    }).compile();
+    }).compile()
 
-    controller = module.get<RedirectionController>(RedirectionController);
-  });
+    controller = module.get<RedirectionController>(RedirectionController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
