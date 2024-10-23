19d9d9a74bbba224d317e9242c8c47a344c3fbd3
Islam Yamor <islamyamor@gmail.com>
2024-10-23 22:08:30 +0300

    [api] created a standalone jwt-strategy module

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,27 +1,12 @@
 import { Module } from '@nestjs/common'
-import { JwtModule } from '@nestjs/jwt'
 import { AuthService } from './auth.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserEntity } from '../users/user.entity'
 import { AuthController } from './auth.controller'
 import { UsersModule } from '@/modules/users/users.module'
-import { EnvironmentVars } from '@/types/environment-vars'
-import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forFeature([UserEntity]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService<EnvironmentVars, true>) => ({
-        global: true,
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: configService.get('JWT_EXPIRY') },
-      }),
-    }),
-  ],
+  imports: [UsersModule, TypeOrmModule.forFeature([UserEntity])],
   providers: [AuthService],
   controllers: [AuthController],
 })
